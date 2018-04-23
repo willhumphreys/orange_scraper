1709d21c4c1ea9d630350aab51669f187d809395
Will Humphreys <whumphreys@gmail.com>
2018-04-23 16:16:05 +0200

    Update the test to use environmental variables

--- test_sraper.py
+++ test_sraper.py
@@ -1,12 +1,17 @@
 import unittest
+import os
 from scrape import my_handler
-
-def fun(x):
-    return x + 1
 
 class ScraperTest(unittest.TestCase):
 
     def testScraper(self):
-        event = {"forge_api_key" : "1234", "data_url" : "http://hello.there.com", "db_table" : "SentimentTest" }
-        self.assertEqual(fun(3), 4)
-        self.assertEqual(my_handler(event,None))
+        forge_api_key = os.environ.get("forge_api_key", "Not Set")
+        data_url = os.environ.get("data_url", "Not Set")
+        db_table = os.environ.get('db_table', "Not Set")
+
+        print(forge_api_key)
+        print(data_url)
+        print(db_table)
+
+        event = {"forge_api_key" : forge_api_key, "data_url" : data_url, "db_table" : db_table }
+        self.assertEqual(my_handler(event,None), {'persisted_items': 'a small tree'})
